d825fd497d5b0e795fa6b51fe205377658c882a5
velitskylev <velitskylev@gmail.com>
2024-02-04 17:47:22 +0200

    Save fig as pdf

--- fig2_reproduction.py
+++ fig2_reproduction.py
@@ -13,7 +13,7 @@ def calculate_population(model: TwoConnectedSubpopulationsModel, populations: Tw
 
 
 def create_fig_2_common(growth_model_1: GrowthModel, growth_model_2: GrowthModel, min_x: float, max_x: float,
-                        min_y: float, max_y: float) -> None:
+                        min_y: float, max_y: float, title: str) -> None:
     total_population_sizes = []
     m_values = np.linspace(0, 1, 1000)
     n_steps = 200
@@ -31,26 +31,28 @@ def create_fig_2_common(growth_model_1: GrowthModel, growth_model_2: GrowthModel
     plt.plot(m_values, total_population_sizes, color='Red', linewidth=3)
     plt.xlabel("Dispersal rate, m")
     plt.ylabel("Total Population Size")
-    plt.title("Beverton-Holt")
+    plt.title(title)
 
     plt.xlim(min_x, max_x)
     plt.ylim(min_y, max_y)
 
     plt.axhline(y=dash_line_y, color='gray', linestyle='--')
 
+    plt.savefig(f'{title}.pdf', format='pdf')
     plt.show()
 
 
 def fig_2_beverton_holt():
     beverton_holt_model_1 = BevertonHoltModel(1.4)
     beverton_holt_model_2 = BevertonHoltModel(1.1)
-    create_fig_2_common(beverton_holt_model_1, beverton_holt_model_2, 0, 1, 0.4, 0.54)
+    create_fig_2_common(beverton_holt_model_1, beverton_holt_model_2, 0, 1, 0.4, 0.54,
+                        "Beverton Holt")
 
 
 def fig_2_ricker():
     ricker_model_1 = RickerModel(1.3636)
     ricker_model_2 = RickerModel(1.1)
-    create_fig_2_common(ricker_model_1, ricker_model_2, 0, 1, 0.3, 0.45)
+    create_fig_2_common(ricker_model_1, ricker_model_2, 0, 1, 0.3, 0.45, "Ricker")
 
 
 def main():
